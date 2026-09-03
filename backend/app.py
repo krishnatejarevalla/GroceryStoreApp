f02bcb074317240d11ddc8afe7709f3b30556fa1
krishnatejarevalla <krishnatejarevalla@gmail.com>
2026-09-03 16:22:37 +0530

    Create initial Flask application

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Grocery Store App is running!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
